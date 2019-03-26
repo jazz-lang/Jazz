7eda905cb1a52ceb5319a762e309659538dbb85b
Adel Prokurov <adel.prokurov@protonmail.com>
2019-03-26 21:32:11 +0300

    Move instruction listing to .def file

--- TVM/src/main.c
+++ TVM/src/main.c
@@ -1,30 +1,6 @@
-#include <iostream>
-#include <jazz-jit.hpp>
-void *
-malloc(int size);
 
-using namespace jazz_jit;
+#include <instructions.h>
 
-int (*f)(void);
-
-typedef union {
-  double input;
-  long output;
-} conv;
-
-typedef union {
-  long input;
-  double output;
-} conv2;
-
-double
-double_from_long(long i)
-{
-  conv2 c;
-  c.input = i;
-  return c.output;
-}
-
-int main()
-{
-}
+int
+main()
+{}
